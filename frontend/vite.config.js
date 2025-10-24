d3e3765f4041a247c78c2e34660556399f56ba4a
Hala CHAOUAT <hala.chaouat@gmail.com>
2025-10-24 14:26:44 +0200

    33,34: the connection between front and back is done , the configuration for docker to include frontend in the command is fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+    proxy: {
+      '/api': {
+        target: 'http://finfam-backend:8080',
+        changeOrigin: true,
+      },
+    },
+  },
 })
